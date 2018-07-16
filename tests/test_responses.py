a200a42adb7d4c018d89d4ebf1ae02865dd8a780
Chris White <chris@prefect.io>
2018-07-16 09:20:05 -0700

    Add additional test for safety

--- tests/test_responses.py
+++ tests/test_responses.py
@@ -8,6 +8,13 @@ def test_at_mentions_responds(slack):
     assert slack.api_called_with("chat.postMessage", channel="foo")
 
 
+def test_at_mentions_doesnt_respond(slack):
+    atmentions = AtMentions(slack)
+    msg = {"text": "<@UBEE>", "channel": "foo"}
+    atmentions([msg])
+    assert slack.api_not_called()
+
+
 def test_at_mentions_doesnt_respond_if_marvin_tags_himself(slack):
     atmentions = AtMentions(slack)
     msg = {"text": "<@UBEEMJZFX>", "channel": "foo", "user": "UBEEMJZFX"}
