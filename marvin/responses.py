c49c2747f8679b150833cf7cd5fdeaf809433025
Chris White <chris@prefect.io>
2018-07-23 19:59:50 -0700

    Functioning webserver refactor of marvin

--- marvin/responses.py
+++ marvin/responses.py
@@ -1,32 +1,45 @@
+import json
 import random
+import requests
+from apistar.http import Body, Response
+from utilities import say
 
-from .core import Response, register
+
+MARVIN_ID = "UBEEMJZFX"
+quotes = [
+    '"Let’s build robots with Genuine People Personalities," they said. So they tried it out with me. I’m a personality prototype. You can tell, can’t you?',
+    "It’s the people you meet in this job that really get you down.",
+    "This is the sort of thing you lifeforms enjoy, is it?",
+    "Don’t pretend you want to talk to me, I know you hate me.",
+    "I think you ought to know I’m feeling very depressed.",
+    "I would like to say that it is a very great pleasure, honour and privilege for me to talk to you, but I can’t because my lying circuits are all out of commission.",
+    "Incredible. It’s even worse than I thought it would be.",
+    "This will all end in tears, I just know it.",
+    "Here I am, brain the size of a planet, and they ask me to talk to you. Call that job satisfaction? ’Cos I don’t.",
+    "It gives me a headache just trying to think down to your level.",
+    "I’d give you advice, but you wouldn’t listen. No one ever does.",
+    ":marvin:",
+    "Do you want me to sit in a corner and rust, or just fall apart where I'm standing?",
+    "Don't feel you have to take any notice of me, please.",
+]
 
 
-@register
-class AtMentions(Response):
+async def event_handler(data: Body):
+    # for validating your URL with slack
+    json_data = json.loads(data)
+    is_challenge = json_data.get('type') == 'url_verification'
+    if is_challenge:
+        return json_data['challenge']
 
-    ID = "UBEEMJZFX"
-    quotes = [
-        '"Let’s build robots with Genuine People Personalities," they said. So they tried it out with me. I’m a personality prototype. You can tell, can’t you?',
-        "It’s the people you meet in this job that really get you down.",
-        "This is the sort of thing you lifeforms enjoy, is it?",
-        "Don’t pretend you want to talk to me, I know you hate me.",
-        "I think you ought to know I’m feeling very depressed.",
-        "I would like to say that it is a very great pleasure, honour and privilege for me to talk to you, but I can’t because my lying circuits are all out of commission.",
-        "Incredible. It’s even worse than I thought it would be.",
-        "This will all end in tears, I just know it.",
-        "Here I am, brain the size of a planet, and they ask me to talk to you. Call that job satisfaction? ’Cos I don’t.",
-        "It gives me a headache just trying to think down to your level.",
-        "I’d give you advice, but you wouldn’t listen. No one ever does.",
-        ":marvin:",
-        "Do you want me to sit in a corner and rust, or just fall apart where I'm standing?",
-        "Don't feel you have to take any notice of me, please.",
-    ]
+    event = json_data.get('event', {})
+    event_type = event.get('type')
+    if event_type == 'app_mention' or MARVIN_ID in event.get("text", ""):
+        return app_mention(event)
 
-    def reply(self, msg):
-        text = msg.get("text", "")
-        who_spoke = msg.get("user", "")
-        if f"@{self.ID}" in text and who_spoke != self.ID:
-            quote = random.choice(self.quotes)
-            self.say(quote, **msg)
+
+def app_mention(event):
+    who_spoke = event.get("user", "")
+    if who_spoke != MARVIN_ID:
+        quote = random.choice(quotes)
+        say(quote, channel=event.get("channel"), thread_ts=event.get("thread_ts"))
+    return Response("")
--- marvin/standup.py
+++ marvin/standup.py
@@ -1,56 +1,57 @@
+import asyncio
 import datetime
-import logging
 import schedule
+from apistar.http import Body, RequestData, Response
+from utilities import get_dm_channel_id, get_users, say
 
-from .core import Response, register
+
+standup_channel = "CBH18KG8G"
+UPDATES = {}
 
 
-@register
-class Standup(Response):
+async def scheduler():
+    schedule.every().day.at("13:30").do(_pre_standup)
+    schedule.every().day.at("14:00").do(_post_standup)
+    while True:
+        await asyncio.sleep(5)
+        schedule.run_pending()
 
-    standup_channel = "CBH18KG8G"
 
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        schedule.every().day.at("13:30").do(self.pre_standup)  # UTC, 9:30 AM EST
-        schedule.every().day.at("14:00").do(self.standup)
-        self.sent_at = {}
-        self.updates = {}
-        self.users = {}
+async def standup_handler(data: RequestData):
+    payload = data.to_dict()
+    user = payload.get('user_name')
+    update = payload.get("text")
+    UPDATES[user] += update + '\n'
+    return f"Thanks! {user}"
 
-    def _is_weekday(self):
-        now = datetime.datetime.now()
-        day = now.strftime("%A")
-        if day in ["Saturday", "Sunday"]:
-            return False
-        else:
-            return True
 
-    def reply(self, msg):
-        thread_ts = msg.get("thread_ts", "")
-        for name, ts in self.sent_at.items():
-            if ts == thread_ts:
-                self.updates[name] += msg.get("text", "") + "\n"
-
-    def pre_standup(self):
-        if not self._is_weekday():
-            return
-        users = self.get_users()
-        self.updates = {}
-        self.sent_at = {}
-        for name, uid in users.items():
-            msg = self.say(
-                f"Hi {name}! What updates do you have for the team today? Please respond by threading to this message and remember: your response will be shared!",
-                channel=self.get_dm_channel_id(uid),
+def _pre_standup():
+    if not _is_weekday():
+        return
+    users = get_users()
+    for name, uid in users.items():
+        if UPDATES.get(name) is None:
+            msg = say(
+                f"Hi {name}! I haven't heard from you yet; what updates do you have for the team today? Please respond by using the slash command `/standup`,  and remember: your response will be shared!",
+                channel=get_dm_channel_id(uid), mrkdwn="true",
             )
-            self.sent_at[name] = msg["ts"]
-            self.updates[name] = ""
-            self.users[uid] = name
+            UPDATES[name] = ""
 
-    def standup(self):
-        if not self._is_weekday():
-            return
-        public_msg = "<!here> are today's standup updates:\n" + "=" * 30
-        for user, update in self.updates.items():
-            public_msg += f"\n*{user}*: {update}"
-        self.say(public_msg, channel=self.standup_channel, mrkdwn="true")
+
+def _post_standup():
+    if not _is_weekday():
+        return
+    public_msg = "<!here> are today's standup updates:\n" + "=" * 30
+    for user, update in UPDATES.items():
+        public_msg += f"\n*{user}*: {update}"
+    say(public_msg, channel=standup_channel)
+    UPDATE.clear()
+
+
+def _is_weekday():
+    now = datetime.datetime.now()
+    day = now.strftime("%A")
+    if day in ["Saturday", "Sunday"]:
+        return False
+    else:
+        return True
