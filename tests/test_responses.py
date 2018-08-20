158432e8b33011177ff7f5d923dad563fafd2577
Chris White <chris@prefect.io>
2018-08-20 11:01:47 -0700

    Add tests for github feature

--- tests/test_responses.py
+++ tests/test_responses.py
@@ -73,3 +73,67 @@ def test_slash_version_responds(app, token):
     r = app.post("/version", json={"channel": "foo", "token": token})
     assert r.ok
     assert "https://github.com" in r.text
+
+
+def test_github_mentions_works(app, token, monkeypatch):
+    post_event = {
+        "text": "",
+        "bot_id": "BBGMPFDHQ",
+        "channel": "foo",
+        "type": "message",
+        "attachments": [
+            {"text": "hey @cicdw you need to fix marvin", "title_link": "foo://bar"}
+        ],
+    }
+    say = MagicMock()
+    monkeypatch.setattr(
+        marvin.responses, "get_dm_channel_id", lambda *args, **kwargs: "dm_chris"
+    )
+    monkeypatch.setattr(marvin.responses, "say", say)
+    r = app.post("/", json={"event": post_event, "token": token})
+    assert r.ok
+    assert say.call_count == 1
+    assert say.call_args[0] == ("You were mentioned on GitHub @ foo://bar",)
+    assert say.call_args[1]["channel"] == "dm_chris"
+
+
+def test_github_mentions_ignores_non_github_posts(app, token, monkeypatch):
+    post_event = {
+        "text": "",
+        "channel": "foo",
+        "type": "message",
+        "attachments": [
+            {"text": "hey @cicdw you need to fix marvin", "title_link": "foo://bar"}
+        ],
+    }
+    say = MagicMock()
+    monkeypatch.setattr(
+        marvin.responses, "get_dm_channel_id", lambda *args, **kwargs: "dm_chris"
+    )
+    monkeypatch.setattr(marvin.responses, "say", say)
+    r = app.post("/", json={"event": post_event, "token": token})
+    assert r.ok
+    assert say.call_count == 0
+
+
+def test_github_mentions_tells_everyone(app, token, monkeypatch):
+    post_event = {
+        "text": "",
+        "bot_id": "BBGMPFDHQ",
+        "channel": "foo",
+        "type": "message",
+        "attachments": [
+            {
+                "text": "hey @cicdw @jlowin @joshmeek you need to fix marvin",
+                "title_link": "foo://bar",
+            }
+        ],
+    }
+    say = MagicMock()
+    monkeypatch.setattr(
+        marvin.responses, "get_dm_channel_id", lambda *args, **kwargs: "dm_id"
+    )
+    monkeypatch.setattr(marvin.responses, "say", say)
+    r = app.post("/", json={"event": post_event, "token": token})
+    assert r.ok
+    assert say.call_count == 3
