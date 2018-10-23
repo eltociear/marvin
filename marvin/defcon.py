b547bdf2cb530c2d7288e13d83a217354cbd9f13
Chris White <chris@prefect.io>
2018-10-23 16:41:19 -0700

    Tests caught a bug yay

--- marvin/defcon.py
+++ marvin/defcon.py
@@ -8,7 +8,7 @@ from .utilities import get_pins, add_pin, remove_pin, say
 
 
 MARVIN_ID = "UBEEMJZFX"
-defcon_channel = "CDCN7CZDX"  # "CBH18KG8G" # engineering
+defcon_channel = "CBH18KG8G"  # engineering
 levels = {
     1: ":matrixparrot:" * 7
     + "\n"
@@ -23,7 +23,7 @@ def level_updater(old_level, old_pin, extreme_level, extreme_msg, oper):
     if old_level == extreme_level:
         return extreme_msg
     elif old_level is None:
-        return "DEFCON level has not been set yet!"
+        return "DEFCON level has not been set yet! Use `/defcon NUMBER` to set one."
 
     new_level = oper(old_level, 1)
     text = levels.get(new_level, "")
@@ -32,6 +32,7 @@ def level_updater(old_level, old_pin, extreme_level, extreme_msg, oper):
     resp = say(f"*DEFCON LEVEL*: {new_level}\n" + text, channel=defcon_channel)
     data = json.loads(resp.text)
     add_pin(channel=defcon_channel, timestamp=data.get("ts", data.get("timestamp")))
+    return "DEFCON level updated!"
 
 
 async def defcon_handler(data: RequestData):
@@ -66,24 +67,22 @@ async def defcon_handler(data: RequestData):
         resp = say(f"*DEFCON LEVEL*: {new_level}" + msg, channel=defcon_channel)
         data = json.loads(resp.text)
         add_pin(channel=defcon_channel, timestamp=data.get("ts", data.get("timestamp")))
-        return ""
+        return "DEFCON level updated!"
 
     if lower_match.match(update.lower()):
-        level_updater(
+        return level_updater(
             old_level,
             defcon_pin,
             1,
             "DEFCON level is already as low as it can be!",
             operator.sub,
         )
-        return ""
 
     if raise_match.match(update.lower()):
-        level_updater(
+        return level_updater(
             old_level,
             defcon_pin,
             5,
             "DEFCON level is already as high as it can be!",
             operator.add,
         )
-        return ""
--- tests/test_defcon.py
+++ tests/test_defcon.py
@@ -7,16 +7,41 @@ from marvin import defcon
 
 @pytest.mark.parametrize("text", ["5", "raise", "lower"])
 def test_defcon_can_be_updated(app, monkeypatch, token, text):
+    pins = [
+        {
+            "type": "message",
+            "message": {
+                "user": defcon.MARVIN_ID,
+                "ts": "yesterday",
+                "text": "DEFCON LEVEL: 3",
+            },
+        }
+    ]
+    monkeypatch.setattr(defcon, "get_pins", MagicMock(return_value=pins))
     say = MagicMock(return_value=MagicMock(text='{"ts": "yesterday"}'))
     monkeypatch.setattr(defcon, "say", say)
-    monkeypatch.setattr(defcon, "add_pin", say)
-    monkeypatch.setattr(defcon, "get_pins", say)
+    monkeypatch.setattr(defcon, "add_pin", MagicMock())
+    monkeypatch.setattr(defcon, "remove_pin", MagicMock())
+    monkeypatch.setattr(defcon, "get_pins", MagicMock(return_value=pins))
 
     r = app.post(
         "/defcon", json={"token": token, "user_name": "test-user", "text": text}
     )
     assert r.ok
-    assert r.text == ""
+    assert r.text == "DEFCON level updated!"
+
+
+@pytest.mark.parametrize("text", ["raise", "lower"])
+def test_defcon_informatively_complains_if_no_level_set(app, monkeypatch, token, text):
+    monkeypatch.setattr(defcon, "get_pins", MagicMock(return_value=[]))
+
+    r = app.post(
+        "/defcon", json={"token": token, "user_name": "test-user", "text": text}
+    )
+    assert r.ok
+    assert (
+        r.text == "DEFCON level has not been set yet! Use `/defcon NUMBER` to set one."
+    )
 
 
 @pytest.mark.parametrize("text", ["5", "raise", "lower"])
