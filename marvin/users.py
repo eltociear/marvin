4300e4b63d7f32be1b62dd4bdc1543aff07dfa47
Jeremiah Lowin <153965+jlowin@users.noreply.github.com>
2019-10-04 14:41:23 -0400

    Fix return from get_all_users()

--- marvin/users.py
+++ marvin/users.py
@@ -49,7 +49,7 @@ def get_all_users():
     """
     Returns all users in the database
     """
-    return [u.to_dict() for u in USERS.values()]
+    return set(USERS.values())
 
 
 async def schedule_refresh_users():
