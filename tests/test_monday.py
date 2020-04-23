1053707d43a71f28e27e69acabdb2103b9a280d2
kingsley <kingsley@prefect.io>
2020-04-23 00:04:48 -0400

    slack slash command for monday to create new items in the product laundry basket

--- tests/test_monday.py
+++ tests/test_monday.py
@@ -3,7 +3,7 @@ from unittest.mock import MagicMock
 
 
 async def test_slash_monday_responds(app, token, monkeypatch):
-    monkeypatch.setattr(marvin.monday.requests.post, MagicMock())
+    monkeypatch.setattr(marvin.monday.requests, MagicMock())
     r = await app.post(
         "/monday",
         json={"channel": "foo", "username": "bar", "text": "some text", "token": token},
