de410dd58559fca56f0da432147d0009c5d9c604
Chris White <chris@prefect.io>
2020-09-22 08:57:17 -0700

    Remove contest responses

--- marvin/responses.py
+++ marvin/responses.py
@@ -197,28 +197,28 @@ async def public_event_handler(request: Request):
         "UKVFX6N3B",
         "UUY8XPC21",
     ]:
-        user_info = get_user_info(who_spoke, name_only=False)
-        email = user_info.get("user", {}).get("profile", {}).get("email")
-        await promotional_signup(user_id=who_spoke, email=email, platform="Slack")
-        contest_messages = [
-            f"It’s the people you meet in this job that really get you down. You're in the contest anyway, <@{who_spoke}>.",
-            f"This contest is the sort of thing you lifeforms enjoy, is it? I've entered you to win, <@{who_spoke}>.",
-            f"Don’t pretend you want to talk to me, I know you hate me. I'll still enter you in the contest, <@{who_spoke}>.",
-            f"I think you ought to know I’m feeling very depressed. Also, you're in the contest, <@{who_spoke}>.",
-            f"I would like to say that it is a very great pleasure, honour and privilege for me to enter <@{who_spoke}> in this contest, but I can’t because my lying circuits are all out of commission.",
-            f"Incredible. The contest is even worse than I thought it would be. I'll still enter you to win, <@{who_spoke}>",
-            f"This contest will all end in tears, I just know it. You're entered anyway, <@{who_spoke}>.",
-            f"Here I am, brain the size of a planet, and they ask me to enter you in a contest. Call that job satisfaction? ’Cos I don’t. I'll still enter you in the contest though, <@{who_spoke}>",
-            f"It gives me a headache just trying to think down to your level. I'll still enter you in the contest, <@{who_spoke}>.",
-            f"I’d give you advice, but you wouldn’t listen. No one ever does. Good luck in the contest <@{who_spoke}>.",
-            f"Don't feel you have to take any notice of me, please. I'll just enter you in the contest <@{who_spoke}>.",
-            f"Why should I want to make anything up? The contest is bad enough as it is without wanting to invent any more of it. I'll still enter you in it, <@{who_spoke}>",
-        ]
-        public_speak(
-            text=random.choice(contest_messages),
-            channel=event["channel"],
-            thread_ts=event.get("thread_ts"),
-        )
+        #        user_info = get_user_info(who_spoke, name_only=False)
+        #        email = user_info.get("user", {}).get("profile", {}).get("email")
+        #        await promotional_signup(user_id=who_spoke, email=email, platform="Slack")
+        #        contest_messages = [
+        #            f"It’s the people you meet in this job that really get you down. You're in the contest anyway, <@{who_spoke}>.",
+        #            f"This contest is the sort of thing you lifeforms enjoy, is it? I've entered you to win, <@{who_spoke}>.",
+        #            f"Don’t pretend you want to talk to me, I know you hate me. I'll still enter you in the contest, <@{who_spoke}>.",
+        #            f"I think you ought to know I’m feeling very depressed. Also, you're in the contest, <@{who_spoke}>.",
+        #            f"I would like to say that it is a very great pleasure, honour and privilege for me to enter <@{who_spoke}> in this contest, but I can’t because my lying circuits are all out of commission.",
+        #            f"Incredible. The contest is even worse than I thought it would be. I'll still enter you to win, <@{who_spoke}>",
+        #            f"This contest will all end in tears, I just know it. You're entered anyway, <@{who_spoke}>.",
+        #            f"Here I am, brain the size of a planet, and they ask me to enter you in a contest. Call that job satisfaction? ’Cos I don’t. I'll still enter you in the contest though, <@{who_spoke}>",
+        #            f"It gives me a headache just trying to think down to your level. I'll still enter you in the contest, <@{who_spoke}>.",
+        #            f"I’d give you advice, but you wouldn’t listen. No one ever does. Good luck in the contest <@{who_spoke}>.",
+        #            f"Don't feel you have to take any notice of me, please. I'll just enter you in the contest <@{who_spoke}>.",
+        #            f"Why should I want to make anything up? The contest is bad enough as it is without wanting to invent any more of it. I'll still enter you in it, <@{who_spoke}>",
+        #        ]
+        #        public_speak(
+        #            text=random.choice(contest_messages),
+        #            channel=event["channel"],
+        #            thread_ts=event.get("thread_ts"),
+        #        )
         return Response()
 
     message_body = event.get("text", "").replace("“", '"').replace("”", '"')
