c7891494309615b79a54dc472650b94784c98446
kingsley <kingsley@prefect.io>
2020-10-04 21:18:01 -0400

    fixed and added tests for new routes

--- tests/test_monday.py
+++ tests/test_monday.py
@@ -2,11 +2,68 @@ import marvin
 from unittest.mock import MagicMock
 
 
-async def test_slash_monday_responds(app, token, monkeypatch):
+async def test_slash_monday_roadmap_responds(app, token, monkeypatch):
     monkeypatch.setattr("marvin.monday.requests", MagicMock())
     monkeypatch.setattr(marvin.utilities.requests, "post", MagicMock(), raising=False)
     r = await app.post(
-        "/monday",
+        "/monday-roadmap",
+        json={
+            "channel_name": "foo",
+            "user_name": "bar",
+            "text": "some text",
+            "token": token,
+        },
+    )
+    assert r.ok
+    assert (
+        "It gives me a headache just trying to think down to your level, but I have added this to Monday."
+        in r.text
+    )
+
+
+async def test_slash_monday_blogs_responds(app, token, monkeypatch):
+    monkeypatch.setattr("marvin.monday.requests", MagicMock())
+    monkeypatch.setattr(marvin.utilities.requests, "post", MagicMock(), raising=False)
+    r = await app.post(
+        "/monday-blogs",
+        json={
+            "channel_name": "foo",
+            "user_name": "bar",
+            "text": "some text",
+            "token": token,
+        },
+    )
+    assert r.ok
+    assert (
+        "It gives me a headache just trying to think down to your level, but I have added this to Monday."
+        in r.text
+    )
+
+
+async def test_slash_monday_customer_feedback_responds(app, token, monkeypatch):
+    monkeypatch.setattr("marvin.monday.requests", MagicMock())
+    monkeypatch.setattr(marvin.utilities.requests, "post", MagicMock(), raising=False)
+    r = await app.post(
+        "/monday-customer-feedback",
+        json={
+            "channel_name": "foo",
+            "user_name": "bar",
+            "text": "some text",
+            "token": token,
+        },
+    )
+    assert r.ok
+    assert (
+        "It gives me a headache just trying to think down to your level, but I have added this to Monday."
+        in r.text
+    )
+
+
+async def test_slash_monday_any_board_responds(app, token, monkeypatch):
+    monkeypatch.setattr("marvin.monday.requests", MagicMock())
+    monkeypatch.setattr(marvin.utilities.requests, "post", MagicMock(), raising=False)
+    r = await app.post(
+        "/monday-any-board",
         json={
             "channel_name": "foo",
             "user_name": "bar",
