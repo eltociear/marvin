cad80c27feb2700e8cfe4128130846721ed4faa3
Jenny G <40272060+zhen0@users.noreply.github.com>
2020-11-19 16:18:16 -0500

    Add Jenny to Marvin @List

--- marvin/responses.py
+++ marvin/responses.py
@@ -196,6 +196,7 @@ async def public_event_handler(request: Request):
         "UN6FTLFAS",
         "UKVFX6N3B",
         "UUY8XPC21",
+        "ULXMV9SD7" #Jenny
     ]:
         #        user_info = get_user_info(who_spoke, name_only=False)
         #        email = user_info.get("user", {}).get("profile", {}).get("email")
