b2a920b2ca2ef88bd24cc90f6dfc1d16514fcf0d
nicholas <znicholasbrown@gmail.com>
2020-12-10 14:31:35 -0500

    Oops, missed the form

--- marvin/post-as-marvin.py
+++ marvin/post-as-marvin.py
@@ -8,7 +8,7 @@ async def post_as_marvin_handler(request: Request) -> JSONResponse:
     """
     Posts a message to any channel as Marvin
     """
-
+    payload = await request.form()
     text = payload.get("text")
 
     return JSONResponse({"response_type": "in_channel", "text": f"{text}"})
