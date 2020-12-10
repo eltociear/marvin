74aaba6d4396c1a8c15c8bc478189ca91e79c176
nicholas <znicholasbrown@gmail.com>
2020-12-10 14:30:24 -0500

    Add the post as marvin command

---
+++ marvin/post-as-marvin.py
@@ -0,0 +1,14 @@
+from starlette.requests import Request
+from starlette.responses import JSONResponse
+
+from .users import get_all_users
+
+
+async def post_as_marvin_handler(request: Request) -> JSONResponse:
+    """
+    Posts a message to any channel as Marvin
+    """
+
+    text = payload.get("text")
+
+    return JSONResponse({"response_type": "in_channel", "text": f"{text}"})
