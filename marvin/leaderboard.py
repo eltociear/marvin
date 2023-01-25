4ecc6b5817e6582f6842e03d2684fa3a44b113f4
Jimi Dodd-o <jimi@prefect.io>
2023-01-25 14:26:24 -0500

    Try double escape

--- marvin/leaderboard.py
+++ marvin/leaderboard.py
@@ -16,8 +16,8 @@ async def leaderboard_handler(request: Request) -> Response:
     channel = payload.get("channel_id")
 
     # logic for making google query
-    top_match = re.compile("^top($|\\s)(\\d*)\\\s*$").match(update)
-    bottom_match = re.compile("^bottom($|\\s)(\\d*)\s*$").match(update)
+    top_match = re.compile("^top($|\\s)(\\d*)\\s*$").match(update)
+    bottom_match = re.compile("^bottom($|\\s)(\\d*)\\s*$").match(update)
     count_match = re.compile("(^\\d*)\\s*$").match(update)
 
     if top_match:
